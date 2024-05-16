5c723165648ef270035aeedd79f563fd123eec35
Tiziano Di Felice <tizianofelice@gmail.com>
2024-05-16 16:14:59 +0200

    prove aggiuntive

--- script.js
+++ script.js
@@ -25,16 +25,27 @@ bold.addEventListener('click', () => {
 
 // ESERCIZIO 2
 
-const card = document.createElement('div');
-card.classList.add('card');
-card.style.width = '18rem'
-document.body.appendChild(card);
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
 
-card.innerHTML = ` 
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div> 
-`
+document.body.appendChild(wrapper);
+
+function appendCard(){
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.style.width = '18rem'
+    wrapper.appendChild(card);
+
+    card.innerHTML = ` 
+        <img src="https://plus.unsplash.com/premium_photo-1664879492637-276ebb5665bf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div> 
+    `
+}
+
+for(let i of paragraphs){
+    appendCard();
+}
